a3915ba3568f68fc8f0facc2134e21bf1464acec
joe_gittings <joe_gittings>
2008-07-06 17:55:49 +0000

    under development

--- OccleveMobileClientProject/src/org/occleve/mobileclient/dictionary/DictionaryBrowser.java
+++ OccleveMobileClientProject/src/org/occleve/mobileclient/dictionary/DictionaryBrowser.java
@@ -35,9 +35,10 @@ import org.occleve.mobileclient.*;
 public class DictionaryBrowser extends Form
 implements CommandListener,ItemCommandListener,Runnable
 {
-	private static final String DICTIONARY_DB_NAME = "dictDb77";
+	private static final String DICTIONARY_DB_NAME = "dictionaryDb";
 	private static final String DICTIONARY_TABLE_NAME = "CEDICT";
-
+	private static final String INDEX_NAME = "allFieldsIndex";
+	
 	private static final String TRADITIONAL_FIELD_NAME = "traditional";
 	private static final String SIMPLIFIED_FIELD_NAME = "simplified";
 	private static final String PINYIN_FIELD_NAME = "pinyin";
@@ -134,7 +135,16 @@ implements CommandListener,ItemCommandListener,Runnable
         Alert progress = new Alert(null,"Creating database...",null,null);
         OccleveMobileMidlet.getInstance().displayAlert(progress,this);
         progress.setTimeout(Alert.FOREVER);
+
+        Table tbl = createDatabaseAndTable(progress);
     	
+    	progress.setString("Importing dictionary....");
+
+    	importDictionary(tbl,progress);
+    }
+
+    private Table createDatabaseAndTable(Alert progress) throws Exception
+    {
     	Database db = Database.create(DICTIONARY_DB_NAME);
     	Table tbl = new Table(DICTIONARY_TABLE_NAME);
     	
@@ -147,14 +157,22 @@ implements CommandListener,ItemCommandListener,Runnable
     	tbl.addColumn(PINYIN_FIELD_NAME, bm.db.Constants.FT_STRING,255);
     	tbl.addColumn(ENGLISH1_FIELD_NAME, bm.db.Constants.FT_STRING,255);
     	tbl.addColumn(ENGLISH2_FIELD_NAME, bm.db.Constants.FT_STRING,255);
-    	
-    	tbl.createFullTextIndex("pinyinIndex",PINYIN_FIELD_NAME,false);
+
+    	// Create an index across all the columns.
+    	String[] cols = {TRADITIONAL_FIELD_NAME,SIMPLIFIED_FIELD_NAME,PINYIN_FIELD_NAME,
+    						ENGLISH1_FIELD_NAME,ENGLISH2_FIELD_NAME};
+    	tbl.createFullTextIndex(INDEX_NAME,cols,false);
     	
         progress.setString("Creating table...");
-    	db.createTable(tbl);
+    	db.createTable(tbl);    	
     	
-    	progress.setString("Importing dictionary....");
+    	return tbl;
+    }
 
+    /**Imports the dictionary from the CEDICT flat file on the local filesystem,
+    to the OpenBaseMovil database.*/
+    private void importDictionary(Table tbl,Alert progress) throws Exception
+    {
     	// For now, this should be the UNCOMPRESSED file.
         String sFilename = "file:///root1/cedict_ts.u8";
         
@@ -187,9 +205,15 @@ implements CommandListener,ItemCommandListener,Runnable
         	}
         }
         
-        System.out.println("Max line length = " + iMaxLength);
+        System.out.println("Max line length = " + iMaxLength);    	
     }
-
+    
+    /**Some sample CEDICT lines:
+    With one English meaning:
+    一共 一共 [yi1 gong4] /altogether/
+	With two English meanings:
+    三級跳 三级跳 [san1 ji2 tiao4] /triple jump (athletics)/hop, skip and jump/
+    */
     private void processCedictLine(String sLine,Table tbl,boolean bTrace,int iLineNo)
     throws Exception
     {
@@ -200,22 +224,33 @@ implements CommandListener,ItemCommandListener,Runnable
     	int iDivider2 = sLine.indexOf(' ',iDivider1 + 1);
     	int iDivider3 = sLine.indexOf('[',iDivider2 + 1);
     	int iDivider4 = sLine.indexOf(']',iDivider3 + 1);
-    	
+    	int iDivider5 = sLine.indexOf('/',iDivider4 + 1);
+       	int iDivider6 = sLine.indexOf('/',iDivider5 + 1);
+       	int iDivider7 = sLine.indexOf('/',iDivider6 + 1);
+           	
     	String sTraditional = sLine.substring(0,iDivider1);
     	String sSimplified = sLine.substring(iDivider1 + 1,iDivider2);
     	String sPinyin = sLine.substring(iDivider3 + 1, iDivider4);
+    	String sEnglish1 = sLine.substring(iDivider5 + 1, iDivider6);
+
+    	String sEnglish2;
+    	if (iDivider7!=-1)
+    		sEnglish2 = sLine.substring(iDivider6 + 1, iDivider7);
+    	else
+    		sEnglish2 = null;
     	
     	Row rw = tbl.createRow();
     	
     	rw.setField(TRADITIONAL_FIELD_NAME,sTraditional);
     	rw.setField(SIMPLIFIED_FIELD_NAME,sSimplified);
     	rw.setField(PINYIN_FIELD_NAME,sPinyin);
+    	rw.setField(ENGLISH1_FIELD_NAME,sEnglish1);
+    	rw.setField(ENGLISH2_FIELD_NAME,sEnglish2);
     	rw.save();
 
     	if (bTrace)
     	{
         	System.out.println(iLineNo + ": " + sLine);
-        	System.out.println(sTraditional + ", " + sSimplified + ", " + sPinyin);
         	System.out.flush();
     	}
     }
@@ -235,8 +270,15 @@ implements CommandListener,ItemCommandListener,Runnable
         {
             try
             {
-        		Database db = Database.connect(DICTIONARY_DB_NAME);
+                Alert progress = new Alert(null,"Deleting database...",null,null);
+                OccleveMobileMidlet.getInstance().displayAlert(progress,this);
+                progress.setTimeout(Alert.FOREVER);
+
+            	Database db = Database.connect(DICTIONARY_DB_NAME);
+        		db.start();
         		db.drop();            	
+        		
+        		progress.setString("Database deleted");
             }
             catch (Exception e) {OccleveMobileMidlet.getInstance().onError(e);}
         }
@@ -263,71 +305,105 @@ implements CommandListener,ItemCommandListener,Runnable
          	
             if (item==m_SearchButton)
             {
-            	System.out.println("Search button clicked...");
-            	OccleveMobileMidlet.getInstance().beep();
-                m_SearchButton.setText("Searching...");
-
-        		Database db = Database.connect(DICTIONARY_DB_NAME);
-            	System.out.println("db = " + db);
-            	if (db==null)
-            	{
-            		m_SearchResultsTextField.setString("No database connection");
-                    m_SearchButton.setText(SEARCH_BUTTON_TEXT);
-            		return;
-            	}
-            	else
-            	{
-            		db.start();
-            	}
-                
-                Table tbl = db.getTable(DICTIONARY_TABLE_NAME);
-                System.out.println("tbl = " + tbl);
-            	if (tbl==null)
-            	{
-            		m_SearchResultsTextField.setString("Table not found in database");
-                    m_SearchButton.setText(SEARCH_BUTTON_TEXT);
-            		return;
-            	}
-                
-                RowSet rs = tbl.findFuzzy(PINYIN_FIELD_NAME,m_SearchForTextField.getString());
-                System.out.println("rs = " + rs);
-                
-                System.out.println("Rowset contains " + rs.size() + " rows");
-                
-                StringBuffer sbResults = new StringBuffer();
-
-                if (rs.size()==0)
-                {
-                	sbResults.append("No matches");
-                }
-                else if (rs.size()>5)
-                {
-                	sbResults.append(rs.size() + " results... showing first 5" +
-                						org.occleve.mobileclient.Constants.NEWLINE);
-                }
-
-                int iCount = 0;
-                while (rs.next() && (iCount<5))
-                {
-                	String sTrad = rs.getCurrent().getString(TRADITIONAL_FIELD_NAME);
-                	String sSimp = rs.getCurrent().getString(SIMPLIFIED_FIELD_NAME);
-                	String sPinyin = rs.getCurrent().getString(PINYIN_FIELD_NAME);
-                	
-                	sbResults.append(sTrad);
-                	sbResults.append(" " + sSimp);
-                	sbResults.append(" " + sPinyin);
-                	sbResults.append(org.occleve.mobileclient.Constants.NEWLINE);
-                	
-                	iCount++;
-                }
-                
-                m_SearchResultsTextField.setString(sbResults.toString());
-                m_SearchButton.setText(SEARCH_BUTTON_TEXT);
+            	searchDatabase();
             }
         }
         catch (Exception e) {OccleveMobileMidlet.getInstance().onError(e);}
     }
 
+    /**Called when the user clicks Search.*/
+    private void searchDatabase() throws Exception
+    {
+    	System.out.println("Search button clicked...");
+    	OccleveMobileMidlet.getInstance().beep();
+        m_SearchButton.setText("Searching...");
+
+		Database db = Database.connect(DICTIONARY_DB_NAME);
+    	System.out.println("db = " + db);
+    	if (db==null)
+    	{
+    		m_SearchResultsTextField.setString("No database connection");
+            m_SearchButton.setText(SEARCH_BUTTON_TEXT);
+    		return;
+    	}
+    	else
+    	{
+    		// Need to call start() before using an existing database.
+    		db.start();
+    	}
+        
+        Table tbl = db.getTable(DICTIONARY_TABLE_NAME);
+        System.out.println("tbl = " + tbl);
+    	if (tbl==null)
+    	{
+    		m_SearchResultsTextField.setString("Table not found in database");
+            m_SearchButton.setText(SEARCH_BUTTON_TEXT);
+    		return;
+    	}
+
+    	// See the javadoc for Table.findFuzzy() for the spec for this expression.
+    	String sFieldExpression =
+    		TRADITIONAL_FIELD_NAME + "+" +
+    		SIMPLIFIED_FIELD_NAME + "+" +
+    		PINYIN_FIELD_NAME + "+" +
+    		ENGLISH1_FIELD_NAME + "+" +
+    		ENGLISH2_FIELD_NAME;
+    		    	
+        RowSet rs = tbl.findFuzzy(sFieldExpression,m_SearchForTextField.getString());
+        System.out.println("rs = " + rs);
+        
+        System.out.println("Rowset contains " + rs.size() + " rows");
+        
+        StringBuffer sbResults = new StringBuffer();
+
+        if (rs.size()==0)
+        {
+        	sbResults.append("No matches");
+        }
+        else if (rs.size()>5)
+        {
+        	sbResults.append(rs.size() + " results... showing first 5" +
+        						org.occleve.mobileclient.Constants.NEWLINE);
+        }
+
+        int iCount = 0;
+        while (rs.next() && (iCount<5))
+        {
+        	String sTrad = rs.getCurrent().getString(TRADITIONAL_FIELD_NAME);
+        	String sSimp = rs.getCurrent().getString(SIMPLIFIED_FIELD_NAME);
+        	String sPinyin = rs.getCurrent().getString(PINYIN_FIELD_NAME);
+        	String sEnglish1 = rs.getCurrent().getString(ENGLISH1_FIELD_NAME);
+        	String sEnglish2 = rs.getCurrent().getString(ENGLISH2_FIELD_NAME);
+        	
+        	sbResults.append(sTrad);
+        	sbResults.append(", " + sSimp);
+        	sbResults.append(", " + sPinyin);
+        	sbResults.append(", " + sEnglish1);
+
+        	// Not all entries have a second english meaning.
+        	if (sEnglish2!=null)
+        	{
+        		if (sEnglish2.length()!=0)
+        		{
+        			sbResults.append(", " + sEnglish2);
+        		}
+        	}
+        	
+        	sbResults.append(org.occleve.mobileclient.Constants.NEWLINE);
+        	
+        	iCount++;
+        }
+
+        String sDisplayMe = sbResults.toString();
+        int iMaxLength = m_SearchResultsTextField.getMaxSize();
+        if (sDisplayMe.length() > iMaxLength)
+        {
+        	sDisplayMe = sDisplayMe.substring(0,iMaxLength);
+        }
+        
+        m_SearchResultsTextField.setString(sDisplayMe);
+        m_SearchButton.setText(SEARCH_BUTTON_TEXT);    	
+    }
 
 }
 
